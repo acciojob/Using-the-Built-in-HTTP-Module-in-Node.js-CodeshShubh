ecab1423205b70972d239b81b28e8ce44b847be0
CodeshShubh <codeshshubh@gmail.com>
2024-12-30 04:48:29 +0530

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+// const filePath = process.argv[2];
+
+const filePath = process.argv[2] || path.join(__dirname, 'output.txt');
+
+// if(!filePath){
+//     console.error("Please provide a file path in command line argument");
+// }
+
+fs.readFile(filePath, 'utf-8', (err , data)=>{
+    if(err){
+      console.error(`Error reading file: ${err.message}`);
+    }else{
+      console.log(data);
+    }
+})
+
+const server = http.createServer((req, res)=>{
+   fs.readFile((filePath , 'utf-8',  (err, data)=>{
+    if(err){
+        res.writable(500, {'Content-Type' : 'text/plain'});
+        res.end(err);
+    }else{
+        res.writable(200, {'Content-Type' : 'text/plain'});
+        res.end(data);
+    }
+   }))
+})
+
+const PORT = 3000;
+
+server.listen(PORT, 'localhost', ()=>{
+    console.log(`server is running on https://localhost:${PORT}`);
+})
 // TODO: Create an HTTP server
 
 // TODO: Listen on port 3000
